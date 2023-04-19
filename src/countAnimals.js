dbcf5dd35edf1941fdbf4fb553c97cf3c5262adc
Marcus Vinicius de Jesus Tourinho <viniciustourinho01@gmail.com>
2023-04-19 18:15:27 -0300

    Requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,20 @@
 const data = require('../data/zoo_data');
 
 const countAnimals = (animal) => {
-  // seu código aqui
+  if (!animal) {
+    return data.species.reduce((acc, curr) => {
+      acc[curr.name] = curr.residents.length;
+      return acc;
+    }, {});
+  }
+
+  const { species, sex } = animal;
+  const animals = data.species.find((specie) => specie.name === species);
+
+  if (!sex) {
+    return animals.residents.length;
+  }
+  return animals.residents.filter((specie) => specie.sex === sex).length;
 };
 
 module.exports = countAnimals;
