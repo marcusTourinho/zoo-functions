dfb1740aab726f3bd540da0ca991fc937c6abbd4
Marcus Vinicius de Jesus Tourinho <viniciustourinho01@gmail.com>
2023-04-19 17:48:54 -0300

    Requisito 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,14 @@
 const data = require('../data/zoo_data');
 
-const isManager = (id) => {
-  // seu código aqui
-};
+const isManager = (id) => data.employees.some((employee) => employee.managers.includes(id));
 
 const getRelatedEmployees = (managerId) => {
-  // seu código aqui
+  if (isManager(managerId)) {
+    return data.employees.filter((employee) => employee.managers.includes(managerId))
+      .map((employee) => `${employee.firstName} ${employee.lastName}`);
+  }
+
+  throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
 };
 
 module.exports = { isManager, getRelatedEmployees };
