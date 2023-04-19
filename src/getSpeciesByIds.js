5cf146dd1e32484bd8afe6698cf9f2e5f60d6fb5
Marcus Vinicius de Jesus Tourinho <viniciustourinho01@gmail.com>
2023-04-19 17:11:12 -0300

    Requisito 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,6 @@
 const data = require('../data/zoo_data');
 
-const getSpeciesByIds = (ids) => {
-  
-};
+const getSpeciesByIds = (...ids) =>
+  data.species.filter((specie) => ids.includes(specie.id));
 
 module.exports = getSpeciesByIds;
