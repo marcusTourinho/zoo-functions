1f60df45b7f16ed0f62f7822ce64cb6a31d4f5cc
Marcus Vinicius de Jesus Tourinho <viniciustourinho01@gmail.com>
2023-04-20 17:20:24 -0300

    Requisito 8

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,21 @@
+const en = require('faker/lib/locales/en');
 const data = require('../data/zoo_data');
+const { prices } = data;
 
-const countEntrants = (entrants) => {
-  // seu código aqui
-};
+const countEntrants = (entrants) => (
+  {
+    child: entrants.filter((entrant) => entrant.age < 18).length,
+    adult: entrants.filter((entrant) => entrant.age >= 18 && entrant.age < 50).length,
+    senior: entrants.filter((entrant) => entrant.age >= 50).length,
+  }
+);
 
 const calculateEntry = (entrants) => {
-  // seu código aqui
+  if(!entrants || entrants === 0) return 0;
+  const totalValue = countEntrants(entrants).adult * prices.adult
+    + countEntrants(entrants).child * prices.child
+    + countEntrants(entrants).senior * prices.senior;
+  return parseFloat(totalValue.toFixed(2));
 };
 
 module.exports = { calculateEntry, countEntrants };
