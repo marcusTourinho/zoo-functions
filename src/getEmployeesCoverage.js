b8c68949760500ae01c7ee436c63397c26e7e4ec
Marcus Vinicius de Jesus Tourinho <viniciustourinho01@gmail.com>
2023-04-23 16:08:04 -0300

    Requisito 11 lint corrigido

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,36 +1,35 @@
-const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 const findSpecies = (employeeData) => {
   const animalsId = employeeData.responsibleFor;
   const animalName = [];
   animalsId.forEach((animal) => {
-      animalName.push(data.species.find((specie) => specie.id === animal).name);
+    animalName.push(data.species.find((specie) => specie.id === animal).name);
   });
-  return animalName
-}
+  return animalName;
+};
 
 const findLocations = (animalNames) => {
   const locations = [];
   animalNames.forEach((animal) => {
     locations.push(data.species.find((specie) => specie.name === animal).location);
-  })
+  });
   return locations;
-}
+};
 
 const findEmployee = (employee) => {
   const info = Object.entries(employee)[0][1];
   const employeeData = data.employees
     .find((person) => person.id === info || person.firstName === info || person.lastName === info);
-    if (!employeeData) {
-      throw new Error('Informações inválidas');
-    }
+  if (!employeeData) {
+    throw new Error('Informações inválidas');
+  }
   return {
     id: employeeData.id,
     fullName: `${employeeData.firstName} ${employeeData.lastName}`,
     species: findSpecies(employeeData),
     locations: findLocations(findSpecies(employeeData)),
-  }
+  };
 };
 
 const getAllEmployees = () => {
@@ -41,7 +40,7 @@ const getAllEmployees = () => {
       fullName: `${item.firstName} ${item.lastName}`,
       species: findSpecies(item),
       locations: findLocations(findSpecies(item)),
-    })
+    });
   });
   return allEmployees;
 };
